097fc24e4b8945579e84c2a8e7293adf4969a0c8
Katie <ktpham115@student.hbuhsd.edu>
2025-12-18 19:22:10 +0000

    uhhh

--- Main.java
+++ Main.java
@@ -6,8 +6,10 @@ public class Main
 {
   public static void main(String[] args) throws IOException
   {
-    File file = new File(input.txt);  // this is your problem input as a File object
+    File file = new File("input.txt");  // this is your problem input as a File object
     Scanner sc = new Scanner(file);   // the Scanner object is now reading from the file object
+    int pointAt;
+    
 
 
     int answer = 0;
@@ -16,17 +18,31 @@ public class Main
       String line = sc.nextLine();  // gets the next line of input.  This is like "R802"
 
       // TODO: Write code that gets the turning direction from the line.  This should be "L" or "R"
-
+      String dir = line.substring(0,1);
 
       // TODO: Write code that gets a substring of the line that contains only the numbers
       // For R802, this would be a String variable with the value "802"
-
+      String dirNum = line.substring(1);
 
       // TODO: Using the parseInt method from the Integer class, convert the String "802" into an int
-
+      int x = Integer.parseInt(dirNum);
 
       // TODO: Now that you have the turning direction, and the turning amount, see if the turn results in a 0
+      if(dir.equalsTo("L"))
+      {
+        pointAt -= dirNum;
+        if(x < 0)
+        {
+          pointAt = 99;
+        }
+      }
+      
+      
 
+      if(dir.equalsTo("R"))
+      {
+        pointAt += dirNum;
+      }
 
     }
 
